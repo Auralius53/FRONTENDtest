0d6a1444e6e709b343a28dd2bdd85f7f265b57d7
ncl <111574394+ncelll@users.noreply.github.com>
2022-08-21 10:49:40 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+<body>
+<h2>My first JavaScript</h2>
+    
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+        Click me to display date and time.
+    </button>
+
+
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello Javascript"'>Cick Me!</button>
+
+</body>
